c060ba867a50e78ed1740940426f08b0e58bb0fa
awubis <awubis@gmail.com>
2019-11-19 00:15:21 -0600

    Implemented JSON exporting

--- scrape.py
+++ scrape.py
@@ -1,14 +1,17 @@
-from wordcount import WordCount as wc
+from WordCount import WordCount as wc
 import string
+import json
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-SEARCH_MOVIE = ""
-LIKE_LIST = ["The Lighthouse", "Parasite", "Whiplash"]
-DISLIKE_LIST = []
+LIKE_LIST = ["Avengers End", "The Terminator", "The Matrix"]
+DISLIKE_LIST = ["It Chapter 2", "The Shining", "Doctor Sleep"]
+
+like_dict = dict
+dislike_dict = dict
 
 def getReviews(movieTitle):
 
@@ -48,15 +51,15 @@ def getReviews(movieTitle):
     #Currently ignores reviews with spoilers
     for review in reviewsList:
         reviewText = review.find_element_by_class_name("text").text
-        if (reviewText != ''):
+        if (reviewText == ''):
             #This add the current review text to one big string, reviews, making the string lowercase and getting rid of punctuation
+            #reviews += reviewText.lower().translate(str.maketrans('','',string.punctuation))
+
+            review.find_element_by_class_name("spoiler-warning__control").click()
+            reviewText = review.find_element_by_class_name("text").text
             reviews += reviewText.lower().translate(str.maketrans('','',string.punctuation))
-            #reviews.append(reviewText)
-            print(reviewText)
-            #review.find_element_by_class_name("spoiler-warning__control").click()
-            #reviewText = review.find_element_by_class_name("text").text
-        #print(reviewText)
-        #reviews.append(reviewText)
+
+        print(reviewText)
 
     print(len(reviews))
 
@@ -64,8 +67,22 @@ def getReviews(movieTitle):
 
     return reviews
 
-dictionary = dict
+like_words = ''
 for movie in LIKE_LIST:
-    dictionary = wc.countWords(wc, getReviews(movie))
-    dictionary = wc.sortFreqDict(wc, dictionary)
-    print('Stop')
+    like_words += getReviews(movie)
+
+dictionary = wc.countWords(wc, like_words)
+dictionary = wc.sortFreqDict(wc, dictionary)
+with open("Like.json", 'w') as f:
+    json.dump(dictionary, f)
+
+dislike_words = ''
+for movie in DISLIKE_LIST:
+    dislike_words += getReviews(movie)
+
+dictionary = wc.countWords(wc, dislike_words)
+dictionary = wc.sortFreqDict(wc, dictionary)
+with open("Dislike.json", 'w') as f:
+    json.dump(dictionary, f)
+
+print('Stop')
