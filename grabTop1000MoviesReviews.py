fc33319093f8503c41e7243007ea4c9b4a7ddb12
awubis <awubis@gmail.com>
2019-12-02 12:29:50 -0600

    Changed structure of Movie obj

--- grabTop1000MoviesReviews.py
+++ grabTop1000MoviesReviews.py
@@ -1,6 +1,6 @@
 import json
 import Scrape
-from MovieObj import Movie
+#from MovieObj import Movie
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,18 +15,51 @@ movies = []
 with open("Top1000.json", "r") as f:
     movieTitles = json.load(f)
 
-for movieTitle in movieTitles:
-    movieTitle = movieTitle.split("(")
-    reviews = Scrape.getReviews(movieTitle[0])
-    
-    if reviews != None:
-        reviewDict = Scrape.generateDict(reviews)
-        movie = Movie(movieTitle, reviewDict)
-    else:
-        movie = Movie(movieTitle, None)
-    movies.append(movie)
+"""with open("MoviesAndReviews", "w") as f:
+    #json.dump([], f)
+    for movieTitle in movieTitles:
+        movieTitle = movieTitle.split("(")
+        reviews = Scrape.getReviews(movieTitle[0])
+        
+        if reviews != None:
+            reviewDict = Scrape.generateDict(reviews)
+            #movie = Movie(movieTitle, reviewDict)
+        else:
+            break
+            #movie = Movie(movieTitle, None)
 
-with open("MoviesAndReviews", "w") as f:
-    json.dump(movies, f)
+        movie = {
+            "Title": movieTitle,
+            "Reviews": reviewDict
+        }
+
+        movie = json.dumps(movie)
+        movies.append(movie)
+        json.dump(movies, f)"""
+
+with open("MoviesAndReviews.json", "w") as f:
+    #json.dump([], f)
+    for movieTitle in movieTitles:
+        movieTitle = movieTitle.split("(")
+        reviews = Scrape.getReviews(movieTitle[0])
+        
+        if reviews != None:
+            reviewDict = Scrape.generateDict(reviews)
+            #movie = Movie(movieTitle, reviewDict)
+        else:
+            reviewDict = None
+            #movie = Movie(movieTitle, None)
+
+        movie = {
+            "Title": movieTitle,
+            "Reviews": reviewDict
+        }
+
+        movie = json.dumps(movie)
+        movies.append(movie)
+        json.dump(movies, f)
+
+"""with open("MoviesAndReviews", "w") as f:
+    json.dump(movies, f)"""
     
 
