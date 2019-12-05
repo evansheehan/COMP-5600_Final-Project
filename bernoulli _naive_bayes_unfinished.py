ca2be61bdd240fe816f9d84356eab3eb50cdbcba
DCM95 <dcm0033@auburn.edu>
2019-12-04 21:29:04 -0600

    Added complement and bernoulli bayes

--- bernoulli _naive_bayes_unfinished.py
+++ bernoulli _naive_bayes_unfinished.py
@@ -46,31 +46,39 @@ disliked_2 = next(movie for movie in movies if movie["Title"][0] == d_2)
 disliked_3 = next(movie for movie in movies if movie["Title"][0] == d_3)"""
 
 # populate two new dictionaries of liked and disliked reviews
-liked_dict = {}
-disliked_dict = {}
+vocab = []
+like_count = 3
+dislike_count = 3
+all_movies = liked_movies.append(disliked_movies)
 #distinct_words = {}
 
-for movie in liked_movies:
+for movie in all_movies:
     assert movie["Reviews"] != None, movie["Title"] + " has no reviews."
     reviews = list(movie["Reviews"].items())
     for review in reviews:
         word = review[0]
-        count = review[1]
-        if word in liked_dict:
-            liked_dict[word] = liked_dict.get(word) + count
-        else:
-            liked_dict.update({word: count})
-
-for movie in disliked_movies:
-    assert movie["Reviews"] != None, movie["Title"] + " has no reviews."
-    reviews = list(movie["Reviews"].items())
-    for review in reviews:
-        word = review[0]
-        count = review[1]
-        if word in disliked_dict:
-            disliked_dict[word] = disliked_dict.get(word) + count
-        else:
-            disliked_dict.update({word: count})
+        entries = []
+        entries[0] = word
+        if word in vocab:
+            if movie in liked_movies:
+                entries[1] += 1
+            if word in disliked_movies:
+                entries[2] += 1
+        else: 
+            
+            
+#
+#for movie in disliked_movies:
+#    assert movie["Reviews"] != None, movie["Title"] + " has no reviews."
+#    reviews = list(movie["Reviews"].items())
+#    dislike_count += 1
+#    for review in reviews:
+#        word = review[0]
+#        count = review[1]
+#        if word in disliked_dict:
+#            disliked_dict[word] = disliked_dict.get(word) + count
+#        else:
+#            disliked_dict.update({word: count})
 
 # instantiate comparison variables
 p_like = 0.0
@@ -79,21 +87,21 @@ p_dislike = 0.0
 # add up all the words total in the liked dict and disliked_dict
 vocabulary_sum = 0
 
-like_sum = 0
-for word in liked_dict:
-    like_sum += liked_dict[word] 
-    #like_sum = word[0] + like_sum
+#like_sum = 0
+#for word in liked_dict:
+#    like_sum += liked_dict[word] 
+#    #like_sum = word[0] + like_sum
+#
+#dislike_sum = 0
+#for word in disliked_dict:
+#    dislike_sum += disliked_dict[word]
+#    #dislike_sum = word[0] + dislike_sum
 
-dislike_sum = 0
-for word in disliked_dict:
-    dislike_sum += disliked_dict[word]
-    #dislike_sum = word[0] + dislike_sum
-
-vocabulary_sum = like_sum + dislike_sum
+vocabulary_sum = like_count + dislike_count
 
 # calculate word count difference between liked and disliked and present the two as a ratio
-p_like = math.log1p(like_sum/(vocabulary_sum))
-p_dislike = math.log1p(dislike_sum/(vocabulary_sum))
+p_like = math.log1p(like_count/(vocabulary_sum))
+p_dislike = math.log1p(dislike_count/(vocabulary_sum))
 
 """prob_word_given_like = {}
 prob_word_given_dislike = {}"""
