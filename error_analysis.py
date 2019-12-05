b28c4cb8fc58aaafa0f32f120ada33d66d9c6191
awubis <awubis@gmail.com>
2019-12-05 16:58:31 -0600

    Noted a bug in error_analysis.py

--- error_analysis.py
+++ error_analysis.py
@@ -2,12 +2,13 @@ def generate_error_rate(liked_test_set, disliked_test_set, results):
     total_correct = 0
     for movie in results:
         movie_title = movie[0]
-        if movie[3] > 0:
-            if movie[0] in liked_test_set:
-                total_correct += 1
-        elif movie[3] < 0:
-            if movie[0] in disliked_test_set:
-                total_correct += 1
+        if movie[3] > 0 and movie[0] in liked_test_set:
+            total_correct += 1
+
+        #Something very wrong, this elif statement is NEVER entered
+        #Will never guess dislike on a movie and be correct???
+        elif movie[3] < 0 and movie[0] in disliked_test_set:
+            total_correct += 1
 
     print("Accuracy: " + str(total_correct/(len(liked_test_set) + len(disliked_test_set))))
     return  total_correct/(len(liked_test_set) + len(disliked_test_set))
